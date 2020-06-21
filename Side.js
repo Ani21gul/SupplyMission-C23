434c382f9b6086aa7ed3117a1b108c37aa42d520
Ani21gul <66204810+Ani21gul@users.noreply.github.com>
2020-06-21 12:44:13 +0530

    Add files via upload

---
+++ Side.js
@@ -0,0 +1,21 @@
+class Side{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic: false
+        }
+        this.width = w;
+        this.height = h;    
+        this.body = Bodies.rectangle(x,y,w,h, options);
+        
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        fill("red");
+        rect(0,0, this.width, this.height );
+        pop();
+    }
+}
